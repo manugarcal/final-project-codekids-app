8021d6b8c2f81b1409a79560ca57e651898a6c56
malegarcal <mgarciac@outlook.it>
2021-02-13 21:02:49 -0300

    regresando a valores default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+import CardsIndex from "./Components/CardsIndex";
+import Navbar from "./Components/Navbar";
+import Home from "./views/Home";
+
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+      </Switch>
+      <CardsIndex />
+    </BrowserRouter>
   );
 }
 

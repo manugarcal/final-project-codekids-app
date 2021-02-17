01bcb0fd0a95378ff384b16575d55daa86f5c0e7
Jano6elmar <jano6belmar@gmail.com>
2021-02-17 13:13:49 -0300

    +logros dashboard planetas

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,28 +1,58 @@
 import Cardcomponent from "../Components/Card"
 import "../style.css"
 
+
+
 const Dashboard = () => {
     return (
 
         <>
             <h1> Dashboard</h1>
-            <div className="container-fluid">
+            <div className="container">
                 <div className="row">
                     <div className="b1 col-md-4">
                         Usuario
                         <Cardcomponent
-                            text="ajskdlaljsdjlkasjdlkaslkjdlaskdjlasjkldasljdjkasldljkasjldkasljkdjalksdjasldkasoidaudoiuaoidusaoiduasioduaoisdalcnasljncikjdhaiohdaoiudsaljlkalkajsdlkajdklajdlkauopidualjcam,nfklajhjdklajdklasjljaksdlkasjdklaslkjdasjkldjklasjdaslkdjasjkldjasdjasjlkdjalsdljaslkdasjdlaskjlasdjaslkncm,ankjahds" />
+                            title="Nivel:"
+                            subtitle="Nombre:"
+                            textButton="Actualizar Datos"
+                            text=""
 
+                        />
                     </div>
 
-                    <div className="b2 col-md-4">Logros
-                    <div className="logros card">  </div>
+                    <div className="b2 col-md-4  ">
+                        <h2>Logros</h2>
+                        
+                        <div className="logros card d-flex justify-content-between">
+                            <div className="row d-flex justify-content-between">
+                                <div className="col-12">
+                                    <img src="assets/logros/1.png" className="img-fluid w-25 " />
+                                    <img src="assets/logros/2.png" className="img-fluid w-25" />
+                                    <img src="assets/logros/3.png" className="img-fluid w-25" />
+                                </div>
+                            </div>
+                            <div className="row d-flex justify-content-between">
+                                <div className="col-12">
+                                    <img src="assets/logros/4.png" className="img-fluid w-25" />
+                                    <img src="assets/logros/5.png" className="img-fluid w-25" />
+                                    <img src="assets/logros/6.png" className="img-fluid w-25" />
+                                </div>
+                            </div>
+                            <div className="row d-flex justify-content-between">
+                                <div className="col-12">
+                                    <img src="assets/logros/7.png" className="img-fluid w-25" />
+                                    <img src="assets/logros/8.png" className="img-fluid w-25" />
+                                    <img src="assets/logros/9.png" className="img-fluid w-25" />
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
 
                     <div className="b3 col-md-4">
-                       
-                            <div className="mapa card" >
+                        Mapa
+                    <div className="mapa card" >
 
 
                         </div>

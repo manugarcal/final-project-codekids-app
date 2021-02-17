bc21b4be54ba9d73d6eb15da28f475c1b5260d67
malegarcal <mgarciac@outlook.it>
2021-02-17 10:51:40 -0300

    update resgister form

--- src/views/GettingStarted.jsx
+++ src/views/GettingStarted.jsx
@@ -52,91 +52,85 @@ const GettingStarted = () => {
             
             <div className="container">
 <div className="row gutters">
-<div className="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
-<div className="card h-100">
-	<div className="card-body">
-		<div className="account-settings">
-			<div className="user-profile">
-				<div className="user-avatar">
-					<img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin"/>
-				</div>
-				<h5 className="user-name">Yuki Hayashi</h5>
-				<h6 className="user-email">yuki@Maxwell.com</h6>
-			</div>
-			<div className="about">
-				<h5>About</h5>
-				<p>I'm Yuki. Full Stack Designer I enjoy creating user-centric, delightful and human experiences.</p>
-			</div>
-		</div>
-	</div>
-</div>
-</div>
-<div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 col-12">
+
+<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 <div className="card h-100">
 	<div className="card-body">
 		<div className="row gutters">
 			<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<h6 className="mb-2 text-primary">Personal Details</h6>
+				<h6 className="mb-2 text-primary">Detalles Personales</h6>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="fullName">Full Name</label>
-					<input type="text" className="form-control" id="fullName" placeholder="Enter full name"/>
+					<label for="fullName">Nombre Completo</label>
+					<input type="text" className="form-control" id="fullName" placeholder="Ingrese Nombre Completo"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="eMail">Email</label>
-					<input type="email" className="form-control" id="eMail" placeholder="Enter email ID"/>
+					<label for="userName">Nombre de Usuario</label>
+					<input type="text" className="form-control" id="userName" placeholder="Ingrese Nombre de Usuraio"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="phone">Phone</label>
+					<label for="phone">Telefono</label>
 					<input type="text" className="form-control" id="phone" placeholder="Enter phone number"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="website">Website URL</label>
-					<input type="url" className="form-control" id="website" placeholder="Website url"/>
+					<label for="eMail">Email</label>
+					<input type="email" className="form-control" id="eMail" placeholder="Email"/>
+				</div>
+			</div>
+			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+				<div className="form-group">
+					<label for="password">Contraseña</label>
+					<input type="password" className="form-control" id="password" placeholder="Ingrese Contraseña"/>
+				</div>
+			</div>
+			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+				<div className="form-group">
+					<label for="password">Repetir Contraseña</label>
+					<input type="password" className="form-control" id="password" placeholder="Repetir Contraseña"/>
 				</div>
 			</div>
 		</div>
 		<div className="row gutters">
 			<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-				<h6 className="mt-3 mb-2 text-primary">Address</h6>
+				<h6 className="mt-3 mb-2 text-primary">Direccion</h6>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="Street">Street</label>
-					<input type="name" className="form-control" id="Street" placeholder="Enter Street"/>
+					<label for="Street">Calle</label>
+					<input type="name" className="form-control" id="Street" placeholder="Ingrese calle"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="ciTy">City</label>
-					<input type="name" className="form-control" id="ciTy" placeholder="Enter City"/>
+					<label for="ciTy">Ciudad</label>
+					<input type="name" className="form-control" id="ciTy" placeholder="Ingrese Ciudad"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="sTate">State</label>
-					<input type="text" className="form-control" id="sTate" placeholder="Enter State"/>
+					<label for="sTate">Estado</label>
+					<input type="text" className="form-control" id="sTate" placeholder="Ingrese Estado"/>
 				</div>
 			</div>
 			<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 				<div className="form-group">
-					<label for="zIp">Zip Code</label>
-					<input type="text" className="form-control" id="zIp" placeholder="Zip Code"/>
+					<label for="zIp">Codigo Postal</label>
+					<input type="text" className="form-control" id="zIp" placeholder="Codigo Postal"/>
 				</div>
 			</div>
 		</div>
 		<div className="row gutters">
 			<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 				<div className="text-right">
-					<button type="button" id="submit" name="submit" className="btn btn-secondary">Cancel</button>
-					<button type="button" id="submit" name="submit" className="btn btn-primary">Update</button>
+					<button type="button" id="submit" name="submit" className="btn btn-secondary">Cancelar</button>
+					<button type="button" id="submit" name="submit" className="btn btn-primary">Enviar</button>
 				</div>
 			</div>
 		</div>
